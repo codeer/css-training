f05d61d615bbaa4c20acea0b193ae7be60fe9bfd
bjorn-rombaut-rombit <bjorn.rombaut@rombit.be>
2018-04-04 00:51:00 +0200

    added node-sass

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     plugins: [
         require('autoprefixer'),
-        require('precss'),
+        require('@csstools/postcss-sass'),
         require('cssnano')({
             preset: 'default',
         }),
